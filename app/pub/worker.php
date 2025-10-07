58269fd5d2ed205af798cea010a36b77743e6a9f
Thomas Klein <thomasklein876@gmail.com>
2025-10-07 23:44:06 +0200

    Code cleanup

--- app/pub/worker.php
+++ app/pub/worker.php
@@ -1,23 +1,13 @@
 <?php
+
 require __DIR__ . '/../app/autoload.php';
 
 use Opengento\Frankengento\Model\FrankenPHPHandler;
+
 try {
-
-    for (;;) {
-        $request = \frankenphp_handle_request(function() {
-            FrankenPHPHandler::handleRequest();
-        });
-
-        // Si pas de nouvelle requête, on sort de la boucle
-        if (!$request) {
-            break;
-        }
-
-        // Le worker peut être recyclé ici si nécessaire
-        gc_collect_cycles();
+    while ($request = \frankenphp_handle_request(FrankenPHPHandler::handleRequest(...))) {
+        \gc_collect_cycles();
     }
 } catch (\Throwable $e) {
     echo $e->getMessage() . PHP_EOL;
 }
-
