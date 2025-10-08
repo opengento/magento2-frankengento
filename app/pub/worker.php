68b5f64efc84b48e714f70d429144c7159624bc9
Thomas Klein <thomasklein876@gmail.com>
2025-10-08 23:22:23 +0200

    Simplify logic before we refactor the handler as a service

--- app/pub/worker.php
+++ app/pub/worker.php
@@ -2,12 +2,33 @@
 
 require __DIR__ . '/../app/autoload.php';
 
-use Opengento\Frankengento\Model\FrankenPHPHandler;
+use Magento\Framework\App\State\ReloadProcessorInterface;
+use Opengento\Frankengento\App\Application;
+use Opengento\Frankengento\ObjectManager\ObjectManagerFactory;
 
-try {
-    while ($request = \frankenphp_handle_request(FrankenPHPHandler::handleRequest(...))) {
-        \gc_collect_cycles();
+$objectManagerFactory = new ObjectManagerFactory();
+$objectManager = $objectManagerFactory->create('frontend');
+/** @var Application $app */
+$app = $this->objectManager->create(Application::class, ['objectManager' => $this->objectManager]);
+
+$handler = static function () use ($app): void {
+    $response = $app->launch();
+
+    http_response_code($response->getHttpResponseCode());
+
+    foreach ($response->getHeaders()?->toArray() ?? [] as $fieldName => $value) {
+        header($fieldName . ': ' . $value);
     }
-} catch (\Throwable $e) {
-    echo $e->getMessage() . PHP_EOL;
-}
+
+    echo $response->getBody() ?? '';
+};
+
+$maxRequests = (int)($_SERVER['MAX_REQUESTS'] ?? 0);
+$nbRequests = 0;
+do {
+    $keepRunning = \frankenphp_handle_request($handler);
+    $reloadProcessor = $this->objectManager->get(ReloadProcessorInterface::class);
+    $reloadProcessor->reloadState();
+    $this->objectManager->_resetState();
+    \gc_collect_cycles();
+} while ($keepRunning && !$maxRequests && $nbRequests++ < $maxRequests);
