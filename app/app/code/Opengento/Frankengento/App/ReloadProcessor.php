f0c374abe3716005f1566cd2288a1fb0f3779f96
Thomas KLEIN <tomak@dnd.fr>
2025-10-09 11:31:55 +0200

    Fix missing module declaration file + undefined references

--- app/app/code/Opengento/Frankengento/App/ReloadProcessor.php
+++ app/app/code/Opengento/Frankengento/App/ReloadProcessor.php
@@ -4,29 +4,16 @@ declare(strict_types=1);
 
 namespace Opengento\Frankengento\App;
 
-use Magento\Eav\Model\Config;
 use Magento\Framework\App\State\ReloadProcessorInterface;
-use Magento\Framework\GraphQl\Config\Data;
-use Magento\Framework\ObjectManagerInterface;
-use Magento\Framework\Search\Request\Config as SearchRequestConfig;
 
 class ReloadProcessor implements ReloadProcessorInterface
 {
-    public function __construct(
-        private SearchRequestConfig $searchRequestConfig,
-        private Config $config,
-        private ObjectManagerInterface $objectManager
-    ) {}
-
     /**
      * @inerhitDoc
      */
     public function reloadState(): void
     {
-        // phpstan:ignore "Class Magento\Framework\GraphQl\Config\Data not found."
-        $this->objectManager->get(Data::class)->reinitData();
-        $this->searchRequestConfig->reinitData();
-        //ToDo look for graphql schema generator?
-        $this->config->clearWithoutCleaningCache();
+        // ToDo: we need to investigate this test in order to reset any missing state not handled natively by the framework:
+        // ToDo: @see vendor/magento/magento2-base/dev/tests/integration/framework/Magento/TestFramework/ApplicationStateComparator
     }
 }
